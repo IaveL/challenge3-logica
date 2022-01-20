8b932ee42d5c89aa1f166f100a7effaa8369704c
Igor Avelino <igoravel21@gmail.com>
2022-01-20 18:55:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,32 @@
+//Loop de 1 a 10 com FOR
+
+for(let i = 1; i<=10; i++)
+console.log("Using for(1->10):", i)
+
+//Loop de 10 a 1 com While
+
+let a = 10;
+
+while(a>=1) {
+    console.log("Using while(10->1): ", a)
+    a-=1
+   
+}
+
+//Loop com todos números ímpares até 100 
+
+let b = 1;
+
+while(b<=100) {
+    console.log("Using while(Impares 1->100): ", b)
+    b+=2
+}
+
+//Loop com todos pares até 100
+
+let c = 0;
+
+while(c<=100) {
+    console.log("Using while(Pares 0->100): ", c)
+    c+=2
+}
